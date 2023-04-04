9d485972064cf85d01f83839fd283785c85ce70e
sam <samuel.legouix@orange.com>
2023-04-04 10:44:21 +0200

    add memmgr_runtime.h
    Signed-off-by: slegouix <samuel.legouix@orange.com>

---
+++ sys/include/tinycontainer/memmgr/memmgr_runtime.h
@@ -0,0 +1,28 @@
+/**
+ *
+ * Copyright (C) 2023, Orange.
+ *
+ * Please, refer to the README.md and LICENSE files of TinyContainer
+ *
+ */
+
+/**
+ * @ingroup sys_tinycontainer
+ *
+ * @{
+ *
+ * @file
+ * @brief   API of TinyContainer Memory Manager sub-module for container runtimes sub-module
+ *
+ * @author  Samuel Legouix <samuel.legouix@orange.com>
+ *
+ */
+
+
+#ifndef TINYCONTAINER_MEMMGR_RUNTIME_H
+#define TINYCONTAINER_MEMMGR_RUNTIME_H
+
+#include "tinycontainer/memmgr/memmgr_common.h"
+
+#endif /* TINYCONTAINER_MEMMGR_RUNTIME_H */
+/** @} */
