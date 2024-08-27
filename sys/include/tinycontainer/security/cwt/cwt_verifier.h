0cb010899b2dbac94f3a4db24420cf8cf5974435
slegouix <samuel.legouix@orange.com>
2024-08-27 13:54:37 +0200

    add a cwt skeleton submodule
    Signed-off-by: slegouix <samuel.legouix@orange.com>

---
+++ sys/include/tinycontainer/security/cwt/cwt_verifier.h
@@ -0,0 +1,36 @@
+/**
+ *
+ * Copyright (C) 2024, Orange.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup sys_tinycontainer
+ *
+ * @{
+ *
+ * @file
+ * @brief   API of TinyContainer CWT Verifier sub-module.
+ *
+ * @author  Samuel Legouix <samuel.leoguix@orange.com>
+ *
+ */
+
+#ifndef TINYCONTAINER_SECURITY_CWT_CWT_VERIFIER_H
+#define TINYCONTAINER_SECURITY_CWT_CWT_VERIFIER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//TODO: not yet implemented
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TINYCONTAINER_SECURITY_CWT_CWT_VERIFIER_H*/
+/** @} */
