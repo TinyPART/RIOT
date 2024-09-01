650e77149f0bbf4cab21cc88dda965301098cc8f
slegouix <samuel.legouix@orange.com>
2024-09-01 21:33:01 +0200

    implements security/crypto/crypto.c with none and psa backend. Upgrade security/crypto/crypto.h api and module dependencies.
    Signed-off-by: slegouix <samuel.legouix@orange.com>

--- sys/include/tinycontainer/security/crypto/crypto.h
+++ sys/include/tinycontainer/security/crypto/crypto.h
@@ -32,27 +32,39 @@ extern "C" {
 /** @brief function to generate a mac from a message
   */
 int crypto_mac(const crypto_key_t * shared_key, crypto_algo_t algo,
-               const uint8_t message, size_t message_size,
-               uint8_t * signature, size_t signature_size);
+               const uint8_t * message, size_t message_size,
+               uint8_t * mac, size_t mac_size);
+
+/** @brief function to verify a mac for a message
+  */
+bool crypto_mac_verify(const crypto_key_t * shared_key, crypto_algo_t algo,
+                       const uint8_t * message, size_t message_size,
+                       const uint8_t * mac, size_t mac_size);
 
 /** @brief function to generate a digital signature from a message
   */
 int crypto_sign(const crypto_key_t * private_key, crypto_algo_t algo,
-                const uint8_t message, size_t message_size,
-                uint8_t * mac, size_t mac_size);
+                const uint8_t * message, size_t message_size,
+                uint8_t * signature, size_t signature_size);
+
+/** @brief function to verify a digital signature for a message
+  */
+bool crypto_sign_verify(const crypto_key_t * private_key, crypto_algo_t algo,
+                        const uint8_t * message, size_t message_size,
+                        const uint8_t * signature, size_t signature_size);
 
 /** @brief function to encrypt a message
   */
 int crypto_encrypt(const crypto_key_t * shared_key, crypto_algo_t algo,
                    const uint8_t * iv, size_t iv_size,
-                   const uint8_t cleartext, size_t cleartext_size,
+                   const uint8_t * cleartext, size_t cleartext_size,
                    uint8_t * ciphertext, size_t ciphertext_size);
 
 /** @brief function to decrypt an encrypted message
   */
 int crypto_decrypt(const crypto_key_t * shared_key, crypto_algo_t algo,
                    const uint8_t * iv, size_t iv_size,
-                   const uint8_t ciphertext, size_t ciphertext_size,
+                   const uint8_t * ciphertext, size_t ciphertext_size,
                    uint8_t * cleartext, size_t cleartext_size);
 
 #ifdef __cplusplus
--- sys/tinycontainer/security/crypto/crypto.c
+++ sys/tinycontainer/security/crypto/crypto.c
@@ -22,63 +22,308 @@
 #include "debug.h"
 
 #include "tinycontainer/security/crypto/crypto.h"
+#include "key.h"
+
+#include <string.h>
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+#include "psa/crypto.h"
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
 
 int crypto_mac(const crypto_key_t * shared_key, crypto_algo_t algo,
-               const uint8_t message, size_t message_size,
-               uint8_t * signature, size_t signature_size) {
-#if IS_USED(TINYCONTAINER_SECURITY_CRYPTO_PSA)
-#endif /* TINYCONTAINER_SECURITY_CRYPTO_PSA */
-    //TODO: not yet implemented
-    (void)shared_key;
-    (void)algo;
-    (void)message;
-    (void)message_size;
-    (void)signature;
-    (void)signature_size;
-    return -1;
-}
+               const uint8_t *message, size_t message_size,
+               uint8_t * mac , size_t mac_size) {
 
-int crypto_sign(const crypto_key_t * private_key, crypto_algo_t algo,
-                const uint8_t message, size_t message_size,
-                uint8_t * mac, size_t mac_size) {
-    //TODO: not yet implemented
-    (void)private_key;
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+
+    /* do nothing */
+
+    (void)shared_key;
     (void)algo;
     (void)message;
     (void)message_size;
     (void)mac;
     (void)mac_size;
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only HMAC_SHA_256 is currently supported */
+    if (algo != CRYPTO_ALGO_HMAC_SHA256) {
+        return -1;
+    }
+
+    static size_t HMAC_KEY_LEN = 32;
+    size_t digest_size =
+        PSA_MAC_LENGTH(PSA_KEY_TYPE_HMAC, HMAC_KEY_LEN,
+                       PSA_ALG_HMAC(PSA_ALG_SHA_256));
+    if (digest_size != mac_size) {
+        return -1;
+    }
+
+    psa_key_id_t key_id = shared_key->key_id;
+
+    size_t output_len = 0;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_mac_compute(key_id, PSA_ALG_HMAC(PSA_ALG_SHA_256),
+                             message, message_size, mac, mac_size,
+                             &output_len);
+    if(status != PSA_SUCCESS || output_len != mac_size) {
+        return -1;
+    }
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
     return -1;
+}
+
+bool crypto_mac_verify(const crypto_key_t * shared_key, crypto_algo_t algo,
+                       const uint8_t *message, size_t message_size,
+                       const uint8_t * mac , size_t mac_size) {
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+    /* Let's be happy whatever the mac value is */
+    (void)shared_key;
+    (void)algo;
+    (void)message;
+    (void)message_size;
+    (void)mac;
+    (void)mac_size;
+    return true;
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only HMAC_SHA_256 is currently supported */
+    if (algo != CRYPTO_ALGO_HMAC_SHA256) {
+        return false;
+    }
+
+    static size_t HMAC_KEY_LEN = 32;
+    size_t digest_size =
+        PSA_MAC_LENGTH(PSA_KEY_TYPE_HMAC, HMAC_KEY_LEN,
+                       PSA_ALG_HMAC(PSA_ALG_SHA_256));
+    if (digest_size != mac_size) {
+        return false;
+    }
+
+    psa_key_id_t key_id = shared_key->key_id;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_mac_verify(key_id, PSA_ALG_HMAC(PSA_ALG_SHA_256),
+                            message, message_size, mac, mac_size);
+    if(status != PSA_SUCCESS) {
+        return false;
+    }
+
+    return true;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
+    return false;
+}
+
+int crypto_sign(const crypto_key_t * private_key, crypto_algo_t algo,
+                const uint8_t * message, size_t message_size,
+                uint8_t * signature, size_t signature_size) {
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+
+    /* do nothing */
+
+    (void)private_key;
+    (void)algo;
+    (void)message;
+    (void)message_size;
+    (void)signature;
+    (void)signature_size;
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only ED25519 is currently supported */
+    if (algo != CRYPTO_ALGO_ED25519) {
+        return -1;
+    }
+
+    psa_key_id_t key_id = private_key->key_id;
+
+    size_t signature_len = 0;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_sign_message(key_id, PSA_ALG_PURE_EDDSA,
+                              message, message_size, signature, signature_size,
+                              &signature_len);
+    if(status != PSA_SUCCESS || signature_len != signature_size) {
+        return -1;
+    }
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
+    return -1;
+}
+
+bool crypto_sign_verify(const crypto_key_t * public_key, crypto_algo_t algo,
+                       const uint8_t * message, size_t message_size,
+                       const uint8_t * signature, size_t signature_size) {
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+
+    /* Let's be happy whatever the signature value is */
+
+    (void)public_key;
+    (void)algo;
+    (void)message;
+    (void)message_size;
+    (void)signature;
+    (void)signature_size;
+
+    return true;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only ED25519 is currently supported */
+    if (algo != CRYPTO_ALGO_ED25519) {
+        return false;
+    }
+
+    psa_key_id_t key_id = public_key->key_id;
+
+    size_t signature_len = 0;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_verify_message(key_id, PSA_ALG_PURE_EDDSA,
+                                message, message_size,
+                                signature, signature_size);
+    if(status != PSA_SUCCESS || signature_len != signature_size) {
+        return false;
+    }
+
+    return true;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
+    return false;
 }
 
 int crypto_encrypt(const crypto_key_t * shared_key, crypto_algo_t algo,
                    const uint8_t * iv, size_t iv_size,
-                   const uint8_t cleartext, size_t cleartext_size,
+                   const uint8_t * cleartext, size_t cleartext_size,
                    uint8_t * ciphertext, size_t ciphertext_size) {
-    //TODO: not yet implemented
-    (void)shared_key;
-    (void)algo;
-    (void)iv;
-    (void)iv_size;
-    (void)cleartext;
-    (void)cleartext_size;
-    (void)ciphertext;
-    (void)ciphertext_size;
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+
+    /* just copy cleartext to ciphertext */
+
+    if(cleartext_size != ciphertext_size) {
+        return -1;
+    }
+    memcpy(ciphertext, cleartext, cleartext_size);
+
+    (void) shared_key;
+    (void) algo;
+    (void) iv;
+    (void) iv_size;
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only AES_128_CBC is currently supported */
+    if (algo != CRYPTO_ALGO_AES_128_CBC) {
+        return -1;
+    }
+
+    /* no iv for AES_128_CBC */
+    if(iv != NULL || iv_size != 0) {
+        return -1;
+    }
+
+    psa_key_id_t key_id = shared_key->key_id;
+
+    size_t output_len = 0;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_cipher_encrypt(key_id, PSA_ALG_CBC_NO_PADDING,
+                                cleartext, cleartext_size,
+                                ciphertext, ciphertext_size,
+                                &output_len);
+    if(status != PSA_SUCCESS || output_len != ciphertext_size) {
+        return -1;
+    }
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
     return -1;
 }
 
 int crypto_decrypt(const crypto_key_t * shared_key, crypto_algo_t algo,
                    const uint8_t * iv, size_t iv_size,
-                   const uint8_t ciphertext, size_t ciphertext_size,
+                   const uint8_t * ciphertext, size_t ciphertext_size,
                    uint8_t * cleartext, size_t cleartext_size) {
-    //TODO: not yet implemented
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE)
+
+    /* just copy ciphertext to cleartext */
+    if(ciphertext_size != cleartext_size) {
+        return -1;
+    }
+    memcpy(cleartext, ciphertext, ciphertext_size);
+
     (void)shared_key;
     (void)algo;
     (void)iv;
     (void)iv_size;
-    (void)cleartext;
-    (void)cleartext_size;
-    (void)ciphertext;
-    (void)ciphertext_size;
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_NONE */
+
+#if IS_USED(MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA)
+
+    /* only AES_128_CBC is currently supported */
+    if (algo != CRYPTO_ALGO_AES_128_CBC) {
+        return -1;
+    }
+
+    /* no iv for AES_128_CBC */
+    if(iv != NULL || iv_size != 0) {
+        return -1;
+    }
+
+    psa_key_id_t key_id = shared_key->key_id;
+
+    size_t output_len = 0;
+
+    psa_status_t status = PSA_ERROR_DOES_NOT_EXIST;
+    status = psa_cipher_decrypt(key_id, PSA_ALG_CBC_NO_PADDING,
+                                ciphertext, ciphertext_size,
+                                cleartext, cleartext_size,
+                                &output_len);
+    if(status != PSA_SUCCESS || output_len != cleartext_size) {
+        return -1;
+    }
+
+    return 0;
+
+#endif /* MODULE_TINYCONTAINER_SECURITY_CRYPTO_PSA */
+
     return -1;
 }
