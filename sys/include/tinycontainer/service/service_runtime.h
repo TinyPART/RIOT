c003af7734ad9c92b7c3b651f4933736d642eb85
slegouix <samuel.legouix@orange.com>
2024-12-12 10:40:21 +0100

    make io buffer size configurable
    Signed-off-by: slegouix <samuel.legouix@orange.com>

--- sys/include/tinycontainer/service/service_runtime.h
+++ sys/include/tinycontainer/service/service_runtime.h
@@ -41,6 +41,12 @@
 extern "C" {
 #endif
 
+#ifdef TINYCONTAINER_IO_BUFFER_SIZE
+#define IO_BUFFER_SIZE TINYCONTAINER_IO_BUFFER_SIZE
+#else
+#define IO_BUFFER_SIZE 256
+#endif /* TINYCONTAINER_IO_BUFFER_SIZE */
+
 /** syscall identifier to heartbeat */
 #define SERVICE_SYSCALL_HEARTBEAT   1
 /** syscall identifier to open an endpoint */
@@ -62,12 +68,13 @@ int32_t service_syscall(void);
           runtime
  */
 typedef struct {
-    uint32_t syscall_id;    /**< the syscall operation to perform          */
-    int8_t fd;              /**< the peer endpoint id or the file
-                                 descriptor for io                         */
-    uint8_t size;           /**< the size of the buffer to read, write     */
-    //TODO: buffer size shall be configurable
-    uint8_t buffer[256];    /**< the runtime memory for io operations    */
+    uint32_t syscall_id;               /**< the syscall operation to perform  */
+    int8_t fd;                         /**< the peer endpoint id or the file
+                                            descriptor for io                 */
+    uint8_t size;                      /**< the size of the buffer to read,
+                                            write                             */
+    uint8_t buffer[IO_BUFFER_SIZE];    /**< the runtime memory for io
+                                            operations                        */
 } service_io_t;
 
 /**
